46a9eaffe31522a396df62dd51822f9058cc33ec
bowon <asdfg3678@gmail.com>
2018-03-28 19:21:31 +0900

    Create Project

---
+++ app/src/main/java/bowonlee/my/dearphotograph/MainActivity.java
@@ -0,0 +1,13 @@
+package bowonlee.my.dearphotograph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
