366dbc0cbf8d63adccf6083e0c80dda5bffddb6d
bowon <asdfg3678@gmail.com>
2018-03-29 18:25:18 +0900

    AutofitTextureView 추가와 Camera에서 사용할 변수들 추가 v1

--- app/src/main/java/bowonlee/my/dearphotograph/CameraPreview.java
+++ app/src/main/java/bowonlee/my/dearphotograph/CameraPreview.java
@@ -1,6 +1,18 @@
 package bowonlee.my.dearphotograph;
 
+import android.content.Context;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
 import android.util.SparseIntArray;
+import android.view.Surface;
+
+import java.io.File;
+import java.util.concurrent.Semaphore;
 
 /**
  * Created by bowon on 2018-03-28.
@@ -11,6 +23,13 @@ public class CameraPreview {
     private static final String TAG = "Camera2Preview";
 
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     /*Camrara Status*/
     private static final int STATE_PREVIEW = 0 ;
@@ -23,9 +42,41 @@ public class CameraPreview {
     private static final int MAX_PREVIEW_WIDTH = 1920;
     private static final int MAX_PREVIEW_HEIGHT = 1080;
 
-    public CameraPreview(){
+    /*CameraComponent*/
+    private String mCameraId;
+    private CameraCaptureSession mCaptureSession;
+    private CameraDevice mCameraDevice;
+    private CaptureRequest mPreviewRequest;
+    private CaptureRequest.Builder mPreviewRequestBuilder;
 
 
+    /*PreviewComponent*/
+    private Size mPreviewSize;
+
+
+    /*For BackgroundThread*/
+    private HandlerThread mBacHandlerThread;
+    private Handler mBackroundHandler;
+
+    /*Syncronizing Semapore*/
+    private Semaphore mCameraOpenCloseLock = new Semaphore(1);
+
+    private boolean mFlashSupported;
+
+    /*For StillImageCapture */
+    private ImageReader mImageReader;
+    private File mFile;
+
+    /*Use Activity */
+    private Context mContext;
+
+    /*Callback Method*/
+
+
+
+
+    public CameraPreview(Context context){
+        mContext = context;
     }
 
 
