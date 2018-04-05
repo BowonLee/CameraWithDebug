46f73416bfdb2883ec16a446d9742008ae9814a5
bowon <asdfg3678@gmail.com>
2018-04-05 21:15:39 +0900

    이미지 뷰의 배치를 통한 데모뷰는 확인되었다. 큰 이미지의 손실은 눈에 띄지 않는다. 데모이기는 하지만 외형이 별로이다. 필터나 UI 조정이 필요할 것 같다.
    당장 바로 해야할 일
    1. 사진 촬영 및 저장
    2. 이미지뷰 배치를 통한 데모 뷰 확인
    
    추가할 부분들
    1. 선택
    case 1. 겔러리를 통한 이미지 가져오기
    case 2. Custom겔러리의 생성
    현제는 case1 을 데모로 하고 case2를 추후 구현하는 것으로 생각중
    
    2. 가져온 이미지를 Crop하여 보여주고 Crop된 상태의 이미지데이터를 유지하여 가지고 있기

--- app/src/main/java/bowonlee/my/dearphotograph/MainActivity.java
+++ app/src/main/java/bowonlee/my/dearphotograph/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.SurfaceTexture;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -16,6 +18,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.TextureView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
@@ -31,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     private CameraPreview cameraPreview;
     private static final int REQUEST_CAMERA_PERMISSION = 1;
     private static final int LOCATE_PERMISSION =2;
+    private ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         mTextureView = (AutoFitTextureView) findViewById(R.id.camera_preview_session);
+        mImageView = (ImageView)findViewById(R.id.imageview);
         setRequestCameraPermission();
 
     }
@@ -64,7 +70,9 @@ public class MainActivity extends AppCompatActivity {
             cameraPreview.setSurface();
         }
 
-
+        if(mImageView!=null){
+              setmImageView();
+        }
     }
 
     @Override
@@ -114,5 +122,45 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+
+
+    public void setmImageView(){
+        //Bitmap image =
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+
+        options.inSampleSize = 4;
+        options.inJustDecodeBounds = false;
+
+
+        mImageView.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.cafe_demoimage,options));
+
+    }
+
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
 
 }
